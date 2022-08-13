243e1a98e66a087e7fa9dc1ebc16f8f96af02e97
DoHyung Kim <neo5188@gmail.com>
2022-08-13 13:40:15 +0900

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,12 @@
-import React from 'react';
-import { Header, MainSection } from "./components";
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends React.Component {
-  constructor(props)
-  {
-    super(props);
-    this.state = {
-      classname: "light_mode"
-    };
-  }
-
-  render() {
-    return (
-      <div className={this.state.classname} id="App">
-        <Header thememode_callback={() => {
-          if(this.state.classname == "light_mode")
-          {
-            this.setState({ classname: "dark_mode"});
-          }
-          else if(this.state.classname == "dark_mode")
-          {
-            this.setState({classname: "light_mode"});
-          }
-        }}/>
-        <MainSection/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>This is capstone-design project page.</h1>
+    </div>
+  );
 }
 
 export default App;
