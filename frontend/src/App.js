89bc2304e3a7c5cc9dfc45d08bfcec63dfcd0a96
DoHyung Kim <neo5188@gmail.com>
2022-09-22 19:02:29 +0900

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
-        <Route path="/" exact />
+        <Route path="/" exact component={Home} />
       </Routes>
+      <HeroSection />
     </>
   );
 }
--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,21 +1,31 @@
-// rfce
 import React from "react";
+import "../App.css";
 import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
-      <h1>ADVENTURE AWATIS</h1>
-      <p>what are you waiting for?</p>
-      <div className="hero-btn"></div>
-      <Button
-        className="btns"
-        buttonStyle="btn--outline"
-        buttonSize="btn--large"
-      >
-        GET STARTED
-      </Button>
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={console.log("hey")}
+        >
+          WATCH TRAILER <i className="far fa-play-circle" />
+        </Button>
+      </div>
     </div>
   );
 }
