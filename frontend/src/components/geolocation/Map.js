eac342f95ef67a6d6a13275c18eaaf45219ea676
good-jinu <gooday6983@gmail.com>
2022-09-27 17:42:02 +0900

    feat: add infowindow

--- frontend/src/components/geolocation/Map.js
+++ frontend/src/components/geolocation/Map.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import "../../assets/Map.css";
+import "assets/Map.css";
+import { get_places } from "restapi/places";
 
 const { kakao } = window;
 
@@ -8,14 +9,41 @@ function Map() {
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new kakao.maps.LatLng(35.1404032, 129.1026432),
+      center: new kakao.maps.LatLng(35.1380032, 129.1011432),
     };
     let kakaoMap = new kakao.maps.Map(container, options);
-    let markerPosition = new kakao.maps.LatLng(35.1404032, 129.1026432);
-    let marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
-    marker.setMap(kakaoMap);
+    get_places().then(res => {
+      for(let i = 0; i<res.data.length; i++) {
+        let marker = new kakao.maps.Marker({
+          position: new kakao.maps.LatLng(res.data[i].Latitude, res.data[i].Longitude)
+        });
+        marker.setMap(kakaoMap);
+
+        // Add infowindow
+        let infowindow = new kakao.maps.CustomOverlay({
+          clickable: true,
+          content: `<div id="p${res.data[i].PlaceID}" class="infowindow">
+            <h1>${res.data[i].Name}</h1>
+            <h2>${res.data[i].Object} : 6</h2>
+            <button class="close">X</button>
+            </div>`,
+          position: marker.getPosition(),
+          yAnchor: 1.2,
+          zIndex: 1000
+        });
+
+        kakao.maps.event.addListener(marker, 'click', () => {
+          infowindow.setMap(kakaoMap);
+          let clsbtn = document.querySelector(`#p${res.data[i].PlaceID} .close`);
+          clsbtn.addEventListener('click', () => {
+            infowindow.setMap(null);
+          });
+        });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    })
   }, []);
 
   // let currentPosition = [];
