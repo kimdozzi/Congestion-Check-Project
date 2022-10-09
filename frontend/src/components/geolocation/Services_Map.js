251244c198071a8e047e046e96e60913b18aebfc
DoHyung Kim <neo5188@gmail.com>
2022-10-09 18:35:55 +0900

    marker position

--- frontend/src/components/geolocation/Services_Map.js
+++ frontend/src/components/geolocation/Services_Map.js
@@ -12,6 +12,26 @@ function Map() {
     };
     let kakaoMap = new kakao.maps.Map(container, options);
 
+    let map = new kakao.maps.Map(container, options);
+
+    let marker = new kakao.maps.Marker({
+      // 지도 중심좌표에 마커를 생성합니다
+      position: map.getCenter(),
+    });
+    // 지도에 마커를 표시합니다
+    marker.setMap(map);
+
+    // 지도에 클릭 이벤트를 등록합니다
+    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+    kakao.maps.event.addListener(map, "click", function (mouseEvent) {
+      // 클릭한 위도, 경도 정보를 가져옵니다
+      let latlng = mouseEvent.latLng;
+
+      // 마커 위치를 클릭한 위치로 옮깁니다
+      marker.setPosition(latlng);
+      //   latlng.getLat(),latlng.getLng()
+    });
+
     get_places()
       .then((res) => {
         for (let i = 0; i < res.data.length; i++) {
