42c2541d711673bae45615157348d4e2050f3fdd
Dohyung Kim <neo5188@gmail.com>
2022-11-01 10:49:26 +0900

    11/01 original

--- frontend/src/restapi/places.js
+++ frontend/src/restapi/places.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 function get_places() {
-    return axios({
-        method: 'GET',
-        url: '/places'
-    });
+  return axios({
+    method: "GET",
+    url: "/places",
+  });
 }
 
 function post_place(req_data) {
-    return axios({
-        method: 'POST',
-        url: '/postplace',
-        data: req_data
-    })
+  return axios({
+    method: "POST",
+    url: "/postplace",
+    data: req_data,
+  });
 }
 
 export { get_places, post_place };
--- server.py
+++ server.py
@@ -4,16 +4,19 @@ import pymysql
 import os
 import time
 
-ht='localhost'
-pt=3306
-pw=''
+ht = 'localhost'
+pt = 3306
+pw = 'qeqe1212'
+
 
 def db_connect():
     return pymysql.connect(host=ht, port=pt, user='root', passwd=pw, db='congestion_db', charset='utf8')
 
+
 app = Flask(__name__, static_url_path='/', static_folder='build')
 
-@app.route('/places',methods=['GET'])
+
+@app.route('/places', methods=['GET'])
 def get_places():
     conn = db_connect()
     with conn:
@@ -21,7 +24,8 @@ def get_places():
             res = placesinfo.db_to_server_places(conn, cursor)
     return jsonify(res)
 
-@app.route('/postplace',methods=['POST'])
+
+@app.route('/postplace', methods=['POST'])
 def post_places():
     conn = db_connect()
     new_data = request.get_json()
@@ -36,29 +40,33 @@ def post_places():
                 return Response("", status=500)
     return Response("", status=200)
 
+
 @app.route('/congestion', methods=['GET'])
 def get_congestion():
     try:
         pid = request.args.get('placeid')
         if pid == None:
-            return Response("", status=400)   
+            return Response("", status=400)
         conn = db_connect()
         with conn:
             with conn.cursor() as cursor:
-                res = congestioninfo.db_to_server_congestions(conn, cursor, pid)
+                res = congestioninfo.db_to_server_congestions(
+                    conn, cursor, pid)
         return jsonify(res)
     except Exception as e:
         print(e)
-        return Response("",status=500)
+        return Response("", status=500)
 
 
 @app.route('/')
-def index_html(): # 루트에서는 index.html을 response로 보냄
-     return app.send_static_file('index.html')
+def index_html():  # 루트에서는 index.html을 response로 보냄
+    return app.send_static_file('index.html')
+
 
 @app.errorhandler(404)
 def not_found(e):  # SPA 이므로 404 에러는 index.html을 보냄으로써 해결한다.
     return index_html()
+
 
 if __name__ == '__main__':
     app.run(debug=True, port='3000')
